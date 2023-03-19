ed05963cf3f2036be5b5e0d0ef269d17e4fb2c07
noahdominic <noahdmnc@gmail.com>
2023-03-19 15:04:42 +0800

    Revert "refactor: Modularise splitting of date and time"
    This reverts commit 56de14eaa1de93b2ecde163d7c73f42fc6682ad1.

--- src/journal/calculators.rs
+++ src/journal/calculators.rs
@@ -47,11 +47,3 @@ pub(crate) fn get_current_date_from_tz(tz_as_str: &str) -> Result<chrono::DateTi
     return Ok(chrono::Utc::now().with_timezone(&timezone));
 }
 
-
-pub(crate) fn split_date_time(date: &str, timezone: &str) -> (String, usize, String) {
-    let mut date_iter = date.split_whitespace();
-    let current_date_iso = date_iter.next().unwrap().trim().to_string();
-    let current_hour = date_iter.next().unwrap().split(":").next().unwrap().parse::<usize>().unwrap();
-    let timezone_url_ready = timezone.replace("/", "%2F");
-    (current_date_iso, current_hour, timezone_url_ready)
-}
