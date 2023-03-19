e0ac013dde0a5c393dc33cd04042a9921dbf2a66
noahdominic <noahdmnc@gmail.com>
2023-03-19 15:21:34 +0800

    refactor: Move generic query fns to separate mod

--- src/journal/query.rs
+++ src/journal/query.rs
@@ -1,106 +1,4 @@
-use std::io::Write;
-use curl::easy::Easy;
-
-/// Prints a string in with blank line paddings
-macro_rules! lnprint {
-    ($($arg:tt)*) => {{
-        println!();
-        print!($($arg)*);
-    }}
-}
-
-/// Handles a generic query by prompting the user with a question and hint, and
-/// reading their input from the standard input stream, which is returned as a `String`
-///
-/// # Arguments
-///
-/// * `question`: A string slice representing the question to be displayed to the user.
-/// * `hint`: A string slice representing a hint or example value to be displayed to the user.
-///
-/// # Returns
-///
-/// An `Result<String, std::io::Error>` containing the user's input,
-/// or `std::io::Error` if an error occurred while reading the input.
-pub(crate) fn query_for_string(question: &str, hint: &str) -> Result<String, std::io::Error> {
-    // Prompt the user with the question and hint.
-    lnprint!("{} [{}]: ", question, hint);
-    std::io::stdout().flush()?;
-    
-    // Read the user's input from the standard input stream.
-    let mut user_response = String::new();
-    std::io::stdin().read_line(&mut user_response)?;
-
-    // Return the user's input, with the trailing newline trimmed, as an `Ok`.
-    Ok(user_response.trim_end().to_string())
-}
-
-/// Handles a generic query by prompting the user with a question and hint, and
-/// reading their input from the standard input stream, which is returned as a `String`
-///
-/// # Arguments
-///
-/// * `question`: A string slice representing the question to be displayed to the user.
-/// * `hint`: A string slice representing a hint or example value to be displayed to the user.
-///
-/// # Returns
-///
-/// An `Result<String, std::io::Error>` containing the user's input,
-/// or `std::io::Error` if an error occurred while reading the input.
-///
-/// # Note
-/// 
-/// This function is dependent on `query_for_string()`.
-pub(crate) fn query_for_bool(question: &str) -> Result<bool, std::io::Error> {
-    // This is very condensed so here's a more readable summary:
-    // - `query_for_string()` is called and passes `question` and `hint`.
-    // - The returned string is then checked if it's "yes" or "y".
-    // - This yes-ness is wrapped in an `Ok()` and returned
-    let answer = query_for_string(question, "y/N")?.trim().to_lowercase();
-    Ok(answer == "yes" || answer == "y")
-}
-
-/// Handles a generic query by prompting the user with a question and hint, and
-/// reading their input from the standard input stream, which is returned as a `usize`
-///
-/// # Arguments
-///
-/// * `question`: A string slice representing the question to be displayed to the user.
-/// * `hint`: A string slice representing a hint or example value to be displayed to the user.
-///
-/// # Returns
-///
-/// An `Result<usize, std::num::ParseIntError>` containing the parsed input as a `usize`,
-/// or `std::num::ParseIntError` if the input could not be parsed as a `usize`.
-///
-/// # Note
-/// 
-/// This function is dependent on `query_for_string()`.
-pub(crate) fn query_for_usize(question: &str) -> Result<usize, Box<dyn std::error::Error>> {
-    let user_response = query_for_string(question, "1|2|3...")?;
-    Ok(user_response.trim().parse::<usize>()?)
-}
-
-/// Makes a generic API call to the given `url`, using the curl crate's Easy API.
-/// Returns a `Result` containing the response body as a vector of bytes on success,
-/// or an error wrapped in a `Box<dyn std::error::Error>` on failure.
-///
-/// # Arguments
-///
-/// * `url` - A string slice representing the URL to call the API on.
-pub(crate) fn call_api_generic(url: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let mut api_caller = Easy::new();
-    api_caller.url(url)?;
-    let mut api_response_bytes = Vec::new();
-    {
-        let mut transfer = api_caller.transfer();
-        transfer.write_function( |received_data| {
-            api_response_bytes.extend_from_slice(received_data);
-            Ok(received_data.len())
-        })?;
-        transfer.perform()?;
-    }
-    Ok(api_response_bytes)
-}
+pub(crate) mod generic;
 
 /// Queries the user for their current location and returns it as a `String`.
 /// Returns a `Result` containing the location string on success,
@@ -110,7 +8,7 @@ pub(crate) fn call_api_generic(url: &str) -> Result<Vec<u8>, Box<dyn std::error:
 ///
 /// This function takes no arguments.
 pub(crate) fn query_location_from_user() -> Result<String, Box<dyn std::error::Error>> {
-    let location: String = super::query::query_for_string("What's your current location", 
+    let location: String = super::query::generic::query_for_string("What's your current location", 
                                                 "<optional, addresses, here>, <city>") ?;
     Ok(location)
 }
@@ -134,9 +32,9 @@ pub(crate) fn query_desired_datetime_from_user(timezone: &str) -> Result<chrono:
         let current_date = super::calculators::get_current_date_from_tz(&timezone) ?;
         println!("According to your given timezone, it is currently {:?}.", 
             current_date.format("%Y %b %d %H:%M:%S %Z (%:z)").to_string());
-        match super::query::query_for_bool("Is this the timezone you want to use?") ? {
+        match super::query::generic::query_for_bool("Is this the timezone you want to use?") ? {
             true => { return Ok(current_date) },
-            false => { timezone = super::query::query_for_string("What should the timezone be?", "Enter an IANA Timezone ") ?}
+            false => { timezone = super::query::generic::query_for_string("What should the timezone be?", "Enter an IANA Timezone ") ?}
         };
     }
 }
@@ -157,7 +55,7 @@ pub(crate) fn get_location_info_from_api(full_location: &str) -> Result<Vec<supe
                                     .trim() // Removes trailing spaces
                                     .replace(" ", "%20");   // Makes string URL-ready
     let url = format!("https://geocoding-api.open-meteo.com/v1/search?name={city}");
-    let api_response_bytes = super::query::call_api_generic(&url)?;
+    let api_response_bytes = super::query::generic::call_api(&url)?;
     let api_response_native: super::GeoResult = serde_json::from_slice(&api_response_bytes)?;
     Ok(api_response_native.results)
 }
@@ -183,7 +81,7 @@ pub(crate) fn choose_location_from_results(results: &[super::Location]) -> Resul
         for (i, result) in results.iter().enumerate() {
             println!("{}. {}", i + 1, result);
         }
-        let choice: usize = super::query::query_for_usize("Enter the number of the city you're in")?;
+        let choice: usize = super::query::generic::query_for_usize("Enter the number of the city you're in")?;
         if choice > 0 && choice <= results.len() {
             Ok(&results[choice - 1])
         } else {
@@ -191,4 +89,3 @@ pub(crate) fn choose_location_from_results(results: &[super::Location]) -> Resul
         }
     }
 }
-
