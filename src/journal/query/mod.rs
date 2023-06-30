93a6e9e9c3727b4fd4b2ba3e1fe82ef2477df7c5
noahdominic <noahdmnc@gmail.com>
2023-06-30 09:11:06 +0800

    [docs] Fix docs

--- src/journal/query/mod.rs
+++ src/journal/query/mod.rs
@@ -4,7 +4,7 @@ pub(crate) mod user;
 use curl::easy::Easy;
 use std::io::Write;
 
-/// Prints a string in with blank line paddings
+/// Prints a string with blank line paddings.
 macro_rules! lnprint {
     ($($arg:tt)*) => {{
         println!();
@@ -12,84 +12,63 @@ macro_rules! lnprint {
     }}
 }
 
-/// Handles a generic query by prompting the user with a question and hint, and
-/// reading their input from the standard input stream, which is returned as a `String`
+/// Prompts the user with a question and hint, reads the user's input from the standard input stream, and returns it as a string.
 ///
 /// # Arguments
 ///
-/// * `question`: A string slice representing the question to be displayed to the user.
-/// * `hint`: A string slice representing a hint or example value to be displayed to the user.
+/// * `question` - The question to prompt the user.
+/// * `hint` - A hint or additional information for the user.
 ///
 /// # Returns
 ///
-/// An `Result<String, std::io::Error>` containing the user's input,
-/// or `std::io::Error` if an error occurred while reading the input.
+/// Returns the user's input as a string or an `std::io::Error` if an I/O error occurs.
 pub(crate) fn for_string(question: &str, hint: &str) -> Result<String, std::io::Error> {
-    // Prompt the user with the question and hint.
     lnprint!("{} ({}): ", question, hint);
     std::io::stdout().flush()?;
 
-    // Read the user's input from the standard input stream.
     let mut user_response = String::new();
     std::io::stdin().read_line(&mut user_response)?;
 
-    // Return the user's input, with the trailing newline trimmed, as an `Ok`.
     Ok(user_response.trim_end().to_string())
 }
 
-/// Handles a generic query by prompting the user with a question and hint, and
-/// reading their input from the standard input stream, which is returned as a `String`
+/// Prompts the user with a yes/no question, reads the user's input from the standard input stream, and returns `true` if the answer is "yes" or "y", and `false` otherwise.
 ///
 /// # Arguments
 ///
-/// * `question`: A string slice representing the question to be displayed to the user.
-/// * `hint`: A string slice representing a hint or example value to be displayed to the user.
+/// * `question` - The yes/no question to prompt the user.
 ///
 /// # Returns
 ///
-/// An `Result<String, std::io::Error>` containing the user's input,
-/// or `std::io::Error` if an error occurred while reading the input.
-///
-/// # Note
-///
-/// This function is dependent on `query_for_string()`.
+/// Returns `true` if the user's answer is "yes" or "y", and `false` otherwise. Returns an `std::io::Error` if an I/O error occurs.
 pub(crate) fn for_bool(question: &str) -> Result<bool, std::io::Error> {
-    // This is very condensed so here's a more readable summary:
-    // - `query_for_string()` is called and passes `question` and `hint`.
-    // - The returned string is then checked if it's "yes" or "y".
-    // - This yes-ness is wrapped in an `Ok()` and returned
     let answer = for_string(question, "y/N")?.trim().to_lowercase();
     Ok(answer == "yes" || answer == "y")
 }
 
-/// Handles a generic query by prompting the user with a question and hint, and
-/// reading their input from the standard input stream, which is returned as a `usize`
+/// Prompts the user with a question, reads the user's input from the standard input stream, and returns the parsed input as a `usize`.
 ///
 /// # Arguments
 ///
-/// * `question`: A string slice representing the question to be displayed to the user.
-/// * `hint`: A string slice representing a hint or example value to be displayed to the user.
+/// * `question` - The question to prompt the user.
 ///
 /// # Returns
 ///
-/// An `Result<usize, std::num::ParseIntError>` containing the parsed input as a `usize`,
-/// or `std::num::ParseIntError` if the input could not be parsed as a `usize`.
-///
-/// # Note
-///
-/// This function is dependent on `query_for_string()`.
+/// Returns the parsed user input as a `usize` or a boxed error (`Box<dyn std::error::Error>`) if parsing or an `std::io::Error` occurs.
 pub(crate) fn for_usize(question: &str) -> Result<usize, Box<dyn std::error::Error>> {
     let user_response = for_string(question, "[index]")?;
     Ok(user_response.trim().parse::<usize>()?)
 }
 
-/// Makes a generic API call to the given `url`, using the curl crate's Easy API.
-/// Returns a `Result` containing the response body as a vector of bytes on success,
-/// or an error wrapped in a `Box<dyn std::error::Error>` on failure.
+/// Performs an API call to the specified URL and returns the response as a vector of bytes.
 ///
 /// # Arguments
 ///
-/// * `url` - A string slice representing the URL to call the API on.
+/// * `url` - The URL to make the API call to.
+///
+/// # Returns
+///
+/// Returns the API response as a vector of bytes (`Vec<u8>`) or a `curl::Error` if an error occurs during the API call.
 pub(crate) fn call_api(url: &str) -> Result<Vec<u8>, curl::Error> {
     let mut api_caller = Easy::new();
     api_caller.url(url)?;
