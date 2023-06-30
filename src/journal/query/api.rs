d2600ef8a07352def09d583e6f6f30d8f317e4ff
noahdominic <noahdmnc@gmail.com>
2023-06-30 09:10:53 +0800

    [docs] Add docs

--- src/journal/query/api.rs
+++ src/journal/query/api.rs
@@ -1,3 +1,47 @@
+/// Retrieves location information for a given full location string by making a request to a geocoding API.
+///
+/// The function extracts the city from the `full_location` string and performs a geocoding API call to obtain location information.
+///
+/// # Arguments
+///
+/// * `full_location` - A string representing the full location, including the city, state, and country.
+///
+/// # Returns
+///
+/// Returns a `Result` containing the location information as a vector of `Location` structs if successful, or an error of type `Box<dyn std::error::Error>` if any of the operations encounter an error.
+///
+/// # Errors
+///
+/// The function can return an error if any of the following operations fail:
+/// * Extracting the city from the `full_location` string due to an invalid format.
+/// * Making the API call to the geocoding API encounters an error.
+/// * Deserializing the API response into the `GeoResult` type fails.
+///
+/// # Example
+///
+/// ```
+/// use crate::journal::Location;
+///
+/// let full_location = "New York, NY, USA";
+/// let result = get_location_info(full_location);
+///
+/// match result {
+///     Ok(locations) => {
+///         for location in locations {
+///             println!("City: {}", location.city);
+///             println!("State: {}", location.state);
+///             println!("Country: {}", location.country);
+///             println!("Latitude: {}", location.latitude);
+///             println!("Longitude: {}", location.longitude);
+///             println!();
+///         }
+///     }
+///     Err(error) => {
+///         eprintln!("Error occurred while retrieving location information: {}", error);
+///         // Handle the error accordingly
+///     }
+/// }
+/// ```
 pub(crate) fn get_location_info(
     full_location: &str,
 ) -> Result<Vec<crate::journal::Location>, Box<dyn std::error::Error>> {
