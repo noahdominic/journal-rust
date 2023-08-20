9b6df583a6b9fc94dc082a05f9e7174aebdf2d6d
noahdominic <noahdmnc@gmail.com>
2023-08-21 02:19:31 +0800

    api: Implement `calculators::get_direction()`

--- src/journal/calculators.rs
+++ src/journal/calculators.rs
@@ -42,3 +42,24 @@ pub(crate) fn get_path_to_todays_entry(base_dir: std::path::PathBuf) -> Result<S
     // Return the path as a String
     Ok(today_entry_path)
 }
+
+/// Returns the cardinal direction as a string based on the given degrees.
+///
+/// # Arguments
+///
+/// * `degrees` - The degrees to convert to a cardinal direction. Should be between 0 and 360.
+///
+/// # Example
+///
+/// ```
+/// let direction = get_direction(45.0);
+/// assert_eq!(direction, "NE");
+/// ```
+pub(crate) fn get_direction(degrees: f64) -> String {
+    let directions = [
+        "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW",
+        "NW", "NNW",
+    ];
+    let index = ((degrees + 11.25) / 22.5) as usize % 16;
+    return String::from(directions[index]);
+}
