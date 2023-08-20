ab64651a6d4833392e9bf3aaf0e2b7bc741c0066
noahdominic <noahdmnc@gmail.com>
2023-08-21 02:21:05 +0800

    api: Implement `calculators::get_current_date_from_tz_as_str()`

--- src/journal/calculators.rs
+++ src/journal/calculators.rs
@@ -63,3 +63,36 @@ pub(crate) fn get_direction(degrees: f64) -> String {
     let index = ((degrees + 11.25) / 22.5) as usize % 16;
     return String::from(directions[index]);
 }
+
+/// Returns the current date and time in the specified timezone.
+///
+/// # Arguments
+///
+/// * `tz_as_str` - A string representing the timezone to use for formatting the date.
+///   This should be in the format `Area/Location`, where `Area` is the name of a time zone
+///   area (e.g., "America", "Europe", "Asia"), and `Location` is the name of a specific
+///   location within that area (e.g., "New_York", "London", "Tokyo").
+///   This str format is based on the `chrono_tz` crate which is further based on IANA TZs
+///
+/// # Returns
+///
+/// A `Result` containing a
+/// `chrono::DateTime<chrono_tz::Tz>` of the current date and time in the specified timezone
+/// or a `chrono_tz::ParseError` when `tz_as_str` is an invalid IANA timezone name.
+///
+/// # Examples
+///
+/// ```
+/// use chrono::DateTime;
+/// use chrono_tz::Tz;
+///
+/// let tz_str = "America/New_York".to_string();
+/// let current_date: DateTime<Tz> = get_current_date(tz_str).unwrap();
+/// println!("{}", current_date);
+/// ```
+pub(crate) fn get_current_date_from_tz_as_str(
+    tz_as_str: &str,
+) -> Result<chrono::DateTime<chrono_tz::Tz>, chrono_tz::ParseError> {
+    let timezone: chrono_tz::Tz = tz_as_str.parse()?;
+    return Ok(chrono::Utc::now().with_timezone(&timezone));
+}
