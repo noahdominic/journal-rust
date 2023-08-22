75ae227ad9eadf697d5d698dc620c57c70fec3f8
noahdominic <noahdmnc@gmail.com>
2023-08-23 02:54:04 +0800

    git: Add copyright info on source files

--- src/journal/query/mod.rs
+++ src/journal/query/mod.rs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Noah Dominic Miranda Silvio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the European Union Public License version 1.2,
+ * as published by the European Commission.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ *
+ * You should have received a copy of the European Union Public Licence
+ * along with this program. If not, see <https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12>.
+ */
+
 pub(crate) mod api;
 pub(crate) mod user;
 
