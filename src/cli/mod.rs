e031af363e7861876ac0a0089399e5c4aed1be33
Noah Dominic <noahdmnc@gmail.com>
2024-01-23 15:06:35 +0800

    write: Messages for init

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,6 +2,9 @@
 // Licensed under the EUPL v1.2
 
 mod args;
+mod interaction;
+
+use indoc;
 
 /** Calls the appropriate function for each subcommand (`init`, `new`, `open`)
  */
@@ -20,10 +23,41 @@ pub fn handle_main() -> Result<(), Box<dyn std::error::Error>> {
 /** Runs the journal initialisation routine
 */
 fn handle_init() -> Result<(), Box<dyn std::error::Error>> {
-    println!(
-        "--Welcome to Journey!-- 
-        Let's initialise your journal."
-    );
+    println!(indoc::indoc! {r#"
+        --Welcome to Journey!--
+        
+        This command-line interface app is here to help you document your thoughts,
+        experiences, and ideas effortlessly.  Let's get you started :)        
+
+        Let's initialise your journal.
+    "#});
+
+    let _ = interaction::pause()?;
+
+    println!(indoc::indoc! {r#"
+
+    
+    --Set your usual location--
+    
+    Your journal will use your default location to automatically detect your
+    default time zone and to detect the current weather.  This will also be printed
+    in your entries.  To ensure the best results, make sure that the last part of
+    your location is somewhere that is specific enough for accurate time zone and
+    weather data.
+    
+    Don't worry---if your city has the same name as a city elsewhere, like Los
+    Angeles in Los Santos or San Francisco in Cebu, you would be asked to pick
+    which city you meant.
+    
+    Examples:                               
+    Avenida 9 SO - Carchi, Guayaquil........Will list locations named 'Guayaquil';
+                                                user will need to choose.
+    Lor Marzuki, Singapore City.............Only one location named 'Singapore 
+                                                City', which is automatically 
+                                                chosen.
+    Al Quds Open University, Gaza...........Will list locations named 'Gaza';
+                                                user will need to choose.
+    "#});
 
     Ok(())
 }
