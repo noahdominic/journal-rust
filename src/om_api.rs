0406b372a3e163e471ee30d839b798deb869563d
noahdominic <noahdmnc@gmail.com>
2024-01-23 18:09:49 +0800

    write: Finished (?) `om_api::get_location_info` returns `Vec<u8>`

--- src/om_api.rs
+++ src/om_api.rs
@@ -1,6 +1,19 @@
-use crate::core;
+pub(crate) fn get_location_info(query: &str) -> Result<Vec<u8>, curl::Error> {
+    let url = format!("https://geocoding-api.open-meteo.com/v1/search?name={query}");
+    call_api(&url)
+}
 
-pub(super) fn get_location_info(
-    query: &str,
-) -> Result<Vec<core::Location, Box<dyn std::error::Error>> {
+pub(crate) fn call_api(url: &str) -> Result<Vec<u8>, curl::Error> {
+    let mut api_caller = curl::easy::Easy::new();
+    api_caller.url(url)?;
+    let mut api_response_bytes = Vec::new();
+    {
+        let mut transfer = api_caller.transfer();
+        transfer.write_function(|received_data| {
+            api_response_bytes.extend_from_slice(received_data);
+            Ok(received_data.len())
+        })?;
+        transfer.perform()?;
+    }
+    Ok(api_response_bytes)
 }
