b3c9d66e766866357b202f48153c6a1075fee756
Noah Dominic <noah@noahdominic.com>
2024-01-28 01:22:20 +0800

    ref: Move helper str literals to their own enum w/ Display

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,7 +4,51 @@
 mod args;
 mod interaction;
 
-use indoc;
+enum HelperMessage {
+    TutorialWelcome,
+    TutorialLocation,
+    TutorialEditor,
+}
+
+impl std::fmt::Display for HelperMessage{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            HelperMessage::TutorialWelcome => write!(f, r#"
+--Welcome to Journey!--
+
+This command-line interface app is here to help you document your thoughts,
+experiences, and ideas effortlessly.  Let's get you started :)
+
+Let's initialise your journal.
+"#
+            ),
+            HelperMessage::TutorialLocation => write!(f, r#"
+--Set your usual location--
+
+Your journal will use your default location to automatically detect your
+default time zone and to detect the current weather.  This will also be printed
+in your entries.  To ensure the best results, make sure that the last part of
+your location is somewhere that is specific enough for accurate time zone and
+weather data.
+
+Examples:
+Avenida 9 SO - Carchi, Guayaquil........Will list locations named 'Guayaquil';
+                                            user will need to choose.
+Lor Marzuki, Singapore City.............Only one location named 'Singapore
+                                            City', which is automatically
+                                            chosen.
+Al Quds Open University, Gaza...........Will list locations named 'Gaza';
+                                            user will need to choose.
+"#
+            ),
+            HelperMessage::TutorialEditor => write!(f, r#"
+Journey does not use its own text editor and will separately run
+a text editor of your own choosing, like vim, nano, and emacs.
+"#
+            )
+        }
+    }
+}
 
 /** Calls the appropriate function for each subcommand (`init`, `new`, `open`)
  */
@@ -23,40 +67,16 @@ pub fn handle_main() -> Result<(), Box<dyn std::error::Error>> {
 /** Runs the journal initialisation routine
 */
 fn handle_init() -> Result<(), Box<dyn std::error::Error>> {
-    println!(indoc::indoc! {r#"
-        --Welcome to Journey!--
-        
-        This command-line interface app is here to help you document your thoughts,
-        experiences, and ideas effortlessly.  Let's get you started :)
-
-        Let's initialise your journal.
-    "#});
-
+    println!("{}", HelperMessage::TutorialWelcome);
     let _ = interaction::pause()?;
 
-    println!(indoc::indoc! {r#"
-
-    
-    --Set your usual location--
-    
-    Your journal will use your default location to automatically detect your
-    default time zone and to detect the current weather.  This will also be printed
-    in your entries.  To ensure the best results, make sure that the last part of
-    your location is somewhere that is specific enough for accurate time zone and
-    weather data.
-    
-    Examples:                               
-    Avenida 9 SO - Carchi, Guayaquil........Will list locations named 'Guayaquil';
-                                                user will need to choose.
-    Lor Marzuki, Singapore City.............Only one location named 'Singapore 
-                                                City', which is automatically 
-                                                chosen.
-    Al Quds Open University, Gaza...........Will list locations named 'Gaza';
-                                                user will need to choose.
-    "#});
-
     // ask for location
+    println!("\n\n{}", HelperMessage::TutorialLocation);
     let (default_location_string, default_location) = interaction::ask::ask_user_for_location()?;
 
+    // ask for location
+    println!("\n\n{}", HelperMessage::TutorialEditor);
     Ok(())
 }
+
+
